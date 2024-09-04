d0d583f7ca8a313c16d27c9c4af3cc14fabc1a21
Olivier Bourgeois <obourgeois@google.com>
2024-09-04 11:40:37 -0400

    Add built-in tools suppport

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from langchain_google_vertexai import ChatVertexAI
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.tools import tool
+from langchain_core.messages import ToolMessage
 from flask import Flask, request
-from flask_cors import CORS
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
 
-    chat = ChatVertexAI(
+    llm = ChatVertexAI(
         model="gemini-1.5-flash-001",
         temperature=0.5,
         max_tokens=5000,
@@ -16,28 +26,21 @@ def create_app():
         stop=None,
     )
 
+    tools = [add]
+    tools_dict = {"add": add}
+    llm_with_tools = llm.bind_tools(tools)
+
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                """You are an expert in answering questions about Japanese anime and able to give useful recommendations. Do not use emoji.
-
-                You have access to the following function, which you should use as much as possible:
-                - get_anime_info
-                  params: [anime_name]
-
-                To call this function, simply respond with this format, and nothing else:
-                    FUNCTION: get_anime_info
-                    PARAMS:
-                        anime_name: PLACEHOLDER
-
-                Else, reply as normal.""",
+                "You are an expert in answering questions about Japanese anime and able to give useful recommendations. Do not use emoji. Do not do math yourself. Always use tools for that.",
             ),
             MessagesPlaceholder(variable_name="messages"),
         ]
     )
 
-    chain = prompt | chat
+    chain = prompt | llm_with_tools
 
     history = ChatMessageHistory()
 
@@ -50,11 +53,23 @@ def create_app():
 
         ai_response = chain.invoke({"messages": history.messages})
 
-        if ai_response.content.startswith("{"):
-            print("> AI")
-            print(ai_response.content)
-            return ""
+        history.add_ai_message(ai_response)
+        print("> AI")
+        print("> " + ai_response.content)
+        print("> " + str(ai_response.tool_calls))
 
+        if len(ai_response.tool_calls) == 0:
+            return ai_response.content
+
+        for tool_call in ai_response.tool_calls:
+            selected_tool = tools_dict[tool_call["name"].lower()]
+            tool_output = selected_tool.invoke(tool_call["args"])
+            tool_message = ToolMessage(tool_output, tool_call_id=tool_call["id"])
+            history.add_message(tool_message)
+            print("> TOOL")
+            print("> " + str(tool_message))
+
+        ai_response = chain.invoke({"messages": history.messages})
         history.add_ai_message(ai_response.content)
         print("> AI")
         print("> " + ai_response.content)
